eb2c37d47de0405ec64215ebdb2e19c4061e0db6
Giorgi <45801652+Barbaqadze@users.noreply.github.com>
2020-08-31 23:56:56 +0400

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -16,7 +16,7 @@ def get_mac(ip):
      arp_request = scapy.ARP(pdst=ip)                         
      broadcast   = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')       
      arp_request_broadcast = broadcast/arp_request            
-     answered , unanswered = scapy.srp(arp_request_broadcast , timeout=1 , verbose=False)
+     answered , unanswered = scapy.srp(arp_request_broadcast , timeout=2 , verbose=False)
      if len(answered) == 0:
          print("Invalid IP address")
          sys.exit()
